eabd8a0487f1ce90796ea4f0928799a2d5e6f4a0
keheliyax <52384651+keheliyax@users.noreply.github.com>
2019-07-08 18:29:33 -0400

    Update _install.sh

--- _install.sh
+++ _install.sh
@@ -1,4 +1,5 @@
-# !/bin/sh
+# !/bin/bash
+
 set -e
 export GIT_VERSION=`git rev-parse --verify --short HEAD 2>/dev/null|| echo "???"`
 sed -i'' -e "s/__revision__ = '0'/__revision__ = '$GIT_VERSION'/" src/artisanlib/__init__.py
