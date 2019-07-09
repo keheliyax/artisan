2a0e16bdf4fae969f9d03b24f5306a3271c50eb2
keheliyax <52384651+keheliyax@users.noreply.github.com>
2019-07-09 19:20:33 -0400

    Update _install.sh

--- _install.sh
+++ _install.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
 set -e
+source ~/virtualenv/python3.7/bin/activate
 export GIT_VERSION=`git rev-parse --verify --short HEAD 2>/dev/null|| echo "???"`
 sed -i'' -e "s/__revision__ = '0'/__revision__ = '$GIT_VERSION'/" src/artisanlib/__init__.py
 .travis/install-${ARTISAN_OS}.sh
